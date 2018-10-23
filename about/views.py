ca847892b9acaf800b03e060787c7137a05fa27b
edebu <burak.ede24@gmail.com>
2018-10-23 18:10:07 +0300

    Hk.

--- about/views.py
+++ about/views.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def about_view(request):
+
+	context = {
+		"Burak Ede"
+		"Website: burakede.com.tr"
+	}
+
+	return render(request, 'about.html', context)
