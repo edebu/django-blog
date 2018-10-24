8e5acbd3e1d9d24213e7d880fe8bdb289a847d3c
edebu <burak.ede24@gmail.com>
2018-10-24 23:50:22 +0300

    link_2

--- home/views.py
+++ home/views.py
@@ -6,8 +6,8 @@ def home_view(request):
     if request.user.is_authenticated():
         link = AddLink(request.POST or None)
         context = {
-            'isim': 'Barış'
-            'link': 'link'
+            'isim': 'Barış',
+            'link': link,
         }
         
 
