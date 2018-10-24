e05df2c95eaf88c7d0ae1379b82f92f72c1a82e1
edebu <burak.ede24@gmail.com>
2018-10-24 09:01:44 +0300

    views updated

--- home/views.py
+++ home/views.py
@@ -11,3 +11,8 @@ def home_view(request):
             'isim': 'Misafir Kullanıcı'
         }
     return render(request, 'home.html', context)
+
+
+def about_view(request):
+
+	return render(request, 'about.html')
