bb06c90c6dc787c65a52d01021b133c28575f90e
edebu <burak.ede24@gmail.com>
2018-10-24 08:34:18 +0300

    urls settings updated

--- blog/urls.py
+++ blog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from home.views import home_view
-from about.views import about_view
+from home.views import about_view
 from django.conf import settings
 from django.conf.urls.static import static
 
