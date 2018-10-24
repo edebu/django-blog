e7e364c7b217c52bd104896ff718c3a6e3737b8a
edebu <burak.ede24@gmail.com>
2018-10-24 23:56:09 +0300

    admin

--- home/admin.py
+++ home/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import AddLink
 
 
-class AddLinkAdmin(models.ModelAdmin):
+class AddLinkAdmin(admin.ModelAdmin):
 	list_display = ['link']
 	admin.site.register(AddLink, AddLinkAdmin)
